06804becb1054578628a9d28bb454208f622d7a8
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2022-10-02 22:12:58 +0300

    инпуты готовы

--- main.js
+++ main.js
@@ -6,8 +6,37 @@ openPopupButton.addEventListener('click', function () {
 });
 
 let closePopupButton = document.querySelector('.pop-up__close-btn');
+let closePopup = document.querySelector('.pop-up');
 
 closePopupButton.addEventListener('click', function () {
-  let closePopup = document.querySelector('.pop-up');
   closePopup.classList.add('pop-up__close');
 });
+
+let form = document.querySelector('.pop-up__form');
+
+let inputName = form.querySelector('#pop-up__name');
+let inputText = form.querySelector('#pop-up__text');
+let titleProfile = document.querySelector('.profile__title');
+let subtitleProfile = document.querySelector('.profile__subtitle');
+let formBtn = form.querySelector('.pop-up__button');
+
+formBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (inputName.value === '') {
+    titleProfile.textContent = 'Жак-Ив Кусто';
+    closePopup.classList.add('pop-up__close');
+  } else {
+    titleProfile.textContent = inputName.value;
+    closePopup.classList.add('pop-up__close');
+  }
+
+  if (inputText.value === '') {
+    subtitleProfile.textContent = 'Исследователь океана';
+    closePopup.classList.add('pop-up__close');
+  } else {
+    subtitleProfile.textContent = inputText.value;
+    closePopup.classList.add('pop-up__close');
+  }
+});
+
+
