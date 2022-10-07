42b7c21100b3b0ae9c60115eaf84d0d980cf455e
Aleksey Mun <mun.leshka@gmail.com>
2022-10-07 20:02:02 +0300

    hotfix/js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 let openPopupButton = document.querySelector('.profile__edit-btn');
+let form = document.querySelector('.pop-up__form');
+let inputName = form.querySelector('#pop-up__name');
+let inputText = form.querySelector('#pop-up__text');
+let titleProfile = document.querySelector('.profile__title');
+let subtitleProfile = document.querySelector('.profile__subtitle');
 
 openPopupButton.addEventListener('click', function () {
   let openPopup = document.querySelector('.pop-up');
   openPopup.classList.remove('pop-up_close');
+  inputName.value = titleProfile.textContent;
+  inputText.value = subtitleProfile.textContent;
 });
 
 let closePopupButton = document.querySelector('.pop-up__close-btn');
@@ -12,39 +19,27 @@ closePopupButton.addEventListener('click', function () {
   closePopup.classList.add('pop-up_close');
 });
 
-let form = document.querySelector('.pop-up__form');
-
-let inputName = form.querySelector('#pop-up__name');
-let inputText = form.querySelector('#pop-up__text');
-let titleProfile = document.querySelector('.profile__title');
-let subtitleProfile = document.querySelector('.profile__subtitle');
-let formBtn = form.querySelector('.pop-up__button');
-
-formBtn.addEventListener('click', function formSubmitHandler(event) {
+form.addEventListener('submit', function formSubmitHandler(event) {
   event.preventDefault();
   if (inputName.value === '') {
     titleProfile.textContent = 'Жак-Ив Кусто';
-    closePopup.classList.add('pop-up_close');
   } else {
     titleProfile.textContent = inputName.value;
-    closePopup.classList.add('pop-up_close');
   }
 
   if (inputText.value === '') {
     subtitleProfile.textContent = 'Исследователь океана';
-    closePopup.classList.add('pop-up_close');
   } else {
     subtitleProfile.textContent = inputText.value;
-    closePopup.classList.add('pop-up_close');
   }
+  
+  closePopup.classList.add('pop-up_close');
 });
 
-let heart = document.querySelectorAll('.cards__like-btn');
+// let heart = document.querySelectorAll('.cards__like-btn');
 
-for (let i = 0; i < heart.length; i++) {
-  heart[i].addEventListener('click', function() {
-    heart[i].classList.toggle('cards__like-btn_state_active');
-  });
-};
-
-form.addEventListener('submit', formSubmitHandler); 
+// for (let i = 0; i < heart.length; i++) {
+//   heart[i].addEventListener('click', function() {
+//     heart[i].classList.toggle('cards__like-btn_state_active');
+//   });
+// };
