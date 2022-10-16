6734cc20673af98f4deb762a820a52760e897a02
Aleksey Mun <mun.leshka@gmail.com>
2022-10-16 19:52:50 +0300

    добавить карточку + первые 6

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,17 @@ const inputName = form.querySelector('#pop-up__name');
 const inputText = form.querySelector('#pop-up__text');
 const titleProfile = document.querySelector('.profile__title');
 const subtitleProfile = document.querySelector('.profile__subtitle');
-const closePopup = document.querySelectorAll('.pop-up');
+const openPopup = document.querySelector('.pop-up_close');
 const closePopupButton = document.querySelector('.pop-up__close-btn');
 
-const openAddBtn = document.querySelector('.profile__add-button');
-const removeAddBtn = document.querySelector('.pop-up_add-btn');
-
-let closePopupArr = Array.from(closePopup);
-
 openPopupButton.addEventListener('click', function () {
-  closePopupArr[0].classList.remove('pop-up_close');
+  openPopup.classList.remove('pop-up_close');
   inputName.value = titleProfile.textContent;
   inputText.value = subtitleProfile.textContent;
 });
 
-openAddBtn.addEventListener('click', function () {
-  closePopupArr[1].classList.remove('pop-up_add-btn');
-});
-
 closePopupButton.addEventListener('click', function () {
-  closePopupArr[0].classList.add('pop-up_close');
+  openPopup.classList.add('pop-up_close');
 });
 
 form.addEventListener('submit', function formSubmitHandler(event) {
@@ -40,5 +31,73 @@ form.addEventListener('submit', function formSubmitHandler(event) {
     subtitleProfile.textContent = inputText.value;
   }
   
-  closePopupArr[0].classList.add('pop-up_close');
+  openPopup.classList.add('pop-up_close');
+});
+
+// Шесть карточек «из коробки»
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const cardsTemplate = document.querySelector('#item').content;
+const cardsList = document.querySelector('.cards__list');
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardsItem = cardsTemplate.querySelector('.cards__item').cloneNode(true);
+  cardsItem.querySelector('.cards__title').textContent = initialCards[i].name;
+  cardsItem.querySelector('.cards__img').src = initialCards[i].link;
+  cardsItem.querySelector('.cards__img').alt = initialCards[i].name;
+  cardsList.append(cardsItem);
+}
+
+// Форма добавления карточки
+
+const openAddBtn = document.querySelector('.profile__add-button');
+const openAddMenu = document.querySelector('.pop-up_add-btn');
+const closeAddBtn = document.querySelector('.pop-up__close-add-btn');
+const formAdd = document.querySelector('.pop-up__form-add');
+
+openAddBtn.addEventListener('click', function() {
+  openAddMenu.classList.remove('pop-up_add-btn');
+});
+
+closeAddBtn.addEventListener('click', function(evt) {
+  openAddMenu.classList.add('pop-up_add-btn');
+});
+
+const inputTitle = document.querySelector('#pop-up__title');
+const inputLink = document.querySelector('#pop-up__link');
+
+formAdd.addEventListener('submit', function formSubmitHandler(event) {
+  event.preventDefault();
+  const cardsItem = cardsTemplate.querySelector('.cards__item').cloneNode(true);
+  cardsItem.querySelector('.cards__title').textContent = inputTitle.value;
+  cardsItem.querySelector('.cards__img').src = inputLink.value;
+  cardsItem.querySelector('.cards__img').alt = inputTitle.value;
+  cardsList.prepend(cardsItem);
+  openAddMenu.classList.add('pop-up_add-btn');
 });
