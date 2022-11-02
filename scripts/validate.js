0050b54f9ee9d07e52240e70d92c56edb8a88898
Aleksey Mun <mun.leshka@gmail.com>
2022-11-02 18:14:09 +0300

    hotfix

--- scripts/validate.js
+++ scripts/validate.js
@@ -102,3 +102,5 @@ enableValidation({
   errorClass: 'pop-up__span-error',
   inputErrorClass: 'pop-up__input_type_error',
 }); 
+
+// привет, прошу прощения мисскликнул и случайно отправил не успев запушить
